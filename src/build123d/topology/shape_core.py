ee0f642c295f3e1239c7b6c1285957e2bd453517
jdegenstein <jdegenstein@users.noreply.github.com>
2026-06-23 10:49:59 -0500

    Fix pylint error

--- src/build123d/topology/shape_core.py
+++ src/build123d/topology/shape_core.py
@@ -2806,8 +2806,10 @@ def topo_distance_to(
     if not all(isinstance(shape, Shape) for shape in sources):
         raise ValueError("Topological distance requires Shape objects")
 
-    peer_type = sources[0].shape_type
-    if any(shape.shape_type != peer_type for shape in sources):
+    valid_sources = tcast(list[Shape], sources)
+    peer_type = valid_sources[0].shape_type
+
+    if any(shape.shape_type != peer_type for shape in valid_sources):
         raise ValueError("Topological distance requires shapes of the same type")
 
     plural_lut = {
